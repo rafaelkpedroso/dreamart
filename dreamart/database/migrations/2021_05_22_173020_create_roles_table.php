5032cd61fda0acde8754164608cab76561f71c3f
Rafael Kretzer Pedroso <rafaelkpedroso@gmail.com>
2021-05-22 15:56:25 -0300

    Taxonomy models

---
+++ dreamart/database/migrations/2021_05_22_173020_create_roles_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRolesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('roles', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->unsignedBigInteger('iduser');
+            $table->foreign('iduser')->references('id')->on('users');
+            $table->unsignedBigInteger('idplan')->nullable();
+            $table->foreign('idplan')->references('id')->on('plans');
+            $table->string('role');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('roles');
+    }
+}
